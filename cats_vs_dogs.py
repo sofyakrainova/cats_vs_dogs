53860e7baeb7117a3d3418621837f46f9068e7bf
Sofya Krainova <sofya.krainova@gmail.com>
2024-09-04 09:39:56 +0200

    cats_vs_dogs and predict are created

---
+++ cats_vs_dogs.py
@@ -0,0 +1,177 @@
+import matplotlib.pyplot as plt
+import tensorflow as tf
+from tensorflow.keras.preprocessing.image import load_img
+from tensorflow.keras.preprocessing.image import img_to_array
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+import shutil
+import random
+import numpy as np
+import os
+
+base_dir = "all_data/"
+dogs_dir = os.path.join(base_dir, "dogs/")
+cats_dir = os.path.join(base_dir, "cats/")
+
+#print("Sample dog image:")
+#plt.imshow(load_img(f"{os.path.join(dogs_dir, os.listdir(dogs_dir)[0])}"))
+#plt.show()
+
+#print("\nSample cat image:")
+#plt.imshow(load_img(f"{os.path.join(cats_dir, os.listdir(cats_dir)[0])}"))
+#plt.show()
+
+sample_image  = load_img(f"{os.path.join(dogs_dir, os.listdir(dogs_dir)[0])}")
+# Convert the image into its numpy array representation
+sample_array = img_to_array(sample_image)
+print(f"Each image has shape: {sample_array.shape}")
+print(f"The maximum pixel value used is: {np.max(sample_array)}")
+
+def create_train_val_dirs(root_path):
+
+  train_dir = os.path.join(root_path, 'training')
+  validation_dir = os.path.join(root_path, 'validation')
+  os.makedirs(train_dir)
+  os.makedirs(validation_dir)
+
+  # Directory with training cat/dog pictures
+  train_cats_dir = os.path.join(train_dir, 'cats')
+  train_dogs_dir = os.path.join(train_dir, 'dogs')
+  os.makedirs(train_cats_dir)
+  os.makedirs(train_dogs_dir)
+
+  # Directory with validation cat/dog pictures
+  validation_cats_dir = os.path.join(validation_dir, 'cats')
+  validation_dogs_dir = os.path.join(validation_dir, 'dogs')
+  os.makedirs(validation_cats_dir)
+  os.makedirs(validation_dogs_dir)
+
+#try:
+#  create_train_val_dirs(root_path=base_dir)
+#except FileExistsError:
+#  print("You should not be seeing this since the upper directory is removed beforehand")
+
+def split_data(SOURCE_DIR, TRAINING_DIR, VALIDATION_DIR, SPLIT_SIZE):
+  """
+  Splits the data into train and test sets
+
+  Args:
+    SOURCE_DIR (string): directory path containing the images
+    TRAINING_DIR (string): directory path to be used for training
+    VALIDATION_DIR (string): directory path to be used for validation
+    SPLIT_SIZE (float): proportion of the dataset to be used for training
+
+  Returns:
+    None
+  """
+  all_files = os.listdir(SOURCE_DIR)
+  good_files = []
+  for f in all_files:
+    if os.path.getsize(os.path.join(SOURCE_DIR, f))>0:
+      good_files.append(f)
+    else:
+      print(f+" filename is zero length, so ignoring.")
+  source_size = len(good_files)
+  train_files = random.sample(good_files, int(source_size*SPLIT_SIZE))
+  for f in good_files:
+    if f in train_files:
+      shutil.copyfile(os.path.join(SOURCE_DIR, f), os.path.join(TRAINING_DIR, f))
+    else:
+      shutil.copyfile(os.path.join(SOURCE_DIR, f), os.path.join(VALIDATION_DIR, f))
+
+TRAINING_DIR = "all_data/training/"
+VALIDATION_DIR = "all_data/validation/"
+
+TRAINING_CATS_DIR = os.path.join(TRAINING_DIR, "cats/")
+VALIDATION_CATS_DIR = os.path.join(VALIDATION_DIR, "cats/")
+
+TRAINING_DOGS_DIR = os.path.join(TRAINING_DIR, "dogs/")
+VALIDATION_DOGS_DIR = os.path.join(VALIDATION_DIR, "dogs/")
+
+# Define proportion of images used for training
+split_size = .9
+
+#split_data(cats_dir, TRAINING_CATS_DIR, VALIDATION_CATS_DIR, split_size)
+#split_data(dogs_dir, TRAINING_DOGS_DIR, VALIDATION_DOGS_DIR, split_size)
+
+# Training and validation splits
+print(f"There are {len(os.listdir(TRAINING_CATS_DIR))} images of cats for training")
+print(f"There are {len(os.listdir(TRAINING_DOGS_DIR))} images of dogs for training")
+print(f"There are {len(os.listdir(VALIDATION_CATS_DIR))} images of cats for validation")
+print(f"There are {len(os.listdir(VALIDATION_DOGS_DIR))} images of dogs for validation")
+
+def train_val_generators(TRAINING_DIR, VALIDATION_DIR):
+  """
+  Creates the training and validation data generators
+
+  Args:
+    TRAINING_DIR (string): directory path containing the training images
+    VALIDATION_DIR (string): directory path containing the testing/validation images
+
+  Returns:
+    train_generator, validation_generator - tuple containing the generators
+  """
+  train_datagen = ImageDataGenerator( rescale = 1.0/255. )
+  train_generator = train_datagen.flow_from_directory(directory=TRAINING_DIR,
+                                                      batch_size=20,
+                                                      class_mode="binary",
+                                                      target_size=(150, 150))
+
+  validation_datagen = ImageDataGenerator( rescale = 1.0/255. )
+  validation_generator = validation_datagen.flow_from_directory(directory=VALIDATION_DIR,
+                                                                batch_size=20,
+                                                                class_mode="binary",
+                                                                target_size=(150, 150))
+  return train_generator, validation_generator
+
+train_generator, validation_generator = train_val_generators(TRAINING_DIR, VALIDATION_DIR)
+
+model = tf.keras.models.Sequential([
+    # Note the input shape is the desired size of the image 150x150 with 3 bytes color
+    tf.keras.layers.Conv2D(16, (3,3), activation='relu', input_shape=(150, 150, 3)),
+    tf.keras.layers.MaxPooling2D(2,2),
+    tf.keras.layers.Conv2D(32, (3,3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2,2),
+    tf.keras.layers.Conv2D(64, (3,3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2,2),
+    # Flatten the results to feed into a DNN
+    tf.keras.layers.Flatten(),
+    # 512 neuron hidden layer
+    tf.keras.layers.Dense(512, activation='relu'),
+    # Only 1 output neuron. It will contain a value from 0-1 where 0 for 1 class ('cats') and 1 for the other ('dogs')
+    tf.keras.layers.Dense(1, activation='sigmoid')
+])
+
+model.compile(optimizer=tf.keras.optimizers.RMSprop(learning_rate=0.001),
+                loss='binary_crossentropy',
+                metrics=['accuracy'])
+
+history = model.fit(train_generator,
+                    epochs=12,
+                    verbose=1,
+                    validation_data=validation_generator)
+
+acc=history.history['accuracy']
+val_acc=history.history['val_accuracy']
+loss=history.history['loss']
+val_loss=history.history['val_loss']
+
+epochs=range(len(acc)) # Get number of epochs
+
+#------------------------------------------------
+# Plot training and validation accuracy per epoch
+#------------------------------------------------
+plt.plot(epochs, acc, 'r', "Training Accuracy")
+plt.plot(epochs, val_acc, 'b', "Validation Accuracy")
+plt.title('Training and validation accuracy')
+plt.show()
+print("")
+
+#------------------------------------------------
+# Plot training and validation loss per epoch
+#------------------------------------------------
+plt.plot(epochs, loss, 'r', "Training Loss")
+plt.plot(epochs, val_loss, 'b', "Validation Loss")
+plt.show()
+
+# Save the weights
+model.save('gfgModel.h5')
---
+++ predict.py
@@ -0,0 +1,35 @@
+from keras.src.utils.module_utils import tensorflow
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+import numpy as np
+import pandas as pd
+#keras 2.4.3
+#keras-preprocessing 1.1.0
+
+print(tensorflow.keras.__version__)
+TEST_DIR = "."
+
+savedModel=load_model("gfgModel.h5")
+print(savedModel.summary())
+
+test_datagen = ImageDataGenerator(rescale=1/255.)
+
+test_generator = test_datagen.flow_from_directory(TEST_DIR,
+                              classes=['test'],
+                              # don't generate labels
+                              class_mode=None,
+                              # don't shuffle
+                              shuffle=False,
+                              # use same size as in training
+                              target_size=(150, 150),
+                              batch_size= 1)
+
+preds = savedModel.predict(test_generator).flatten().astype(int)
+print(preds)
+
+filenames=test_generator.filenames
+results=pd.DataFrame({"id":filenames,
+                      "labels":preds})
+results.to_csv("submission1.csv",index=False)
+
+
